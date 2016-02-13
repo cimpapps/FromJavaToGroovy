808937099a35e8103697e347bde1cfbe54c31143
cimpapps <catalincimpoeru24@gmail.com>
2016-02-13 15:27:10 +0200

    Exception handling with Groovy

--- FromJavaToGroovy/src/com/cimpapps/part1/chapter1/FromJtoG.groovy
+++ FromJavaToGroovy/src/com/cimpapps/part1/chapter1/FromJtoG.groovy
@@ -1,17 +1,35 @@
 package com.cimpapps.part1.chapter1
 
-class FromJtoG {
+class FromJtoG implements Runnable {
 	
 	static main(args){
-		println printReverse("cimpapps.com")
+		Thread t = new Thread(new FromJtoG());
+		t.start();
 	}//end of main
 		
-	static def printReverse(str){
-		//check if str is null first
-		str?.reverse();
-		//in java we had this:
-		//if(str!=null){return str.reverse();}
+	
+	public void run() {
+		10.times {
+			println "Catalin este cel mai tare"
+			
+			//Groovy doesn't force us to handle checked exception
+			Thread.sleep(1000)
+		}
+		
+		try {
+			openFile("fisiercarenuseEczista")
+		}
+		//See the special catch that can catch all kinds of exceptions
+		catch (ex) {
+			println "Nu se eczista fisierul"
+		}
+		
 	}
 	
-	
+	def openFile(fileName){
+		//we can handle when ever we want with 
+		//FileNotFoundException
+		//no need to throw or try catch in here
+		new FileInputStream(fileName)
+	}
 }//end of class
